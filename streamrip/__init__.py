355a97b28805c6726438f4991daba3abbff90cdd
Muhamad Risman <real.risman@gmail.com>
2026-07-24 08:52:51 +0700

    Bump version to 3.0.2

--- streamrip/__init__.py
+++ streamrip/__init__.py
@@ -2,4 +2,4 @@ from . import converter, db, exceptions, media, metadata
 from .config import Config
 
 __all__ = ["Config", "converter", "db", "exceptions", "media", "metadata"]
-__version__ = "3.0.1"
+__version__ = "3.0.2"
